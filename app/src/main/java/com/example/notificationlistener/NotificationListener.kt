44630de55cefe7bbc32713fedbed1b36b650a89a
Luís Felipe <luis.felipe@smgbit.com.br>
2025-03-13 12:33:07 -0300

    work in inter and nubank

--- app/src/main/java/com/example/notificationlistener/NotificationListener.kt
+++ app/src/main/java/com/example/notificationlistener/NotificationListener.kt
@@ -5,6 +5,7 @@ import android.service.notification.StatusBarNotification
 import android.util.Log
 import com.example.notificationlistener.database.AppDatabase
 import com.example.notificationlistener.database.Notification
+import com.example.notificationlistener.entity.BankNotification
 import com.example.notificationlistener.entity.Expense
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,29 +23,27 @@ class NotificationListener : NotificationListenerService() {
         sbn?.let {
             val notification = it.notification.extras
             val title = notification.getString("android.title") ?: ""
-            val text = notification.getString("android.text") ?: ""
-            val subtext = notification.getString("android.subtext") ?: ""
-            val app = notification.getString("android.appName") ?: ""
+            val text = notification.getString("android.subtext") ?: ""
 
-            Log.d("NotificationListener", title)
-            Log.d("NotificationListener", text)
-            Log.d("NotificationListener", subtext)
-            Log.d("NotificationListener", app)
+            val bankNotification = BankNotification(
+                title,
+                text,
+                timestamp = getCurrentTimestamp(),
+                bank = if(text.contains("com o cartão final"))  "inter" else "nubank"
+            )
 
-            saveNotification(title, text, subtext, app, getCurrentTimestamp())
+            val expense = Expense(
+                name = bankNotification.extractName(),
+                value = bankNotification.extractValue(),
+                "Sem categoria",
+                bankNotification.timestamp
+            )
 
-            if (subtext.contains("APROVADA", ignoreCase = false)) {
-                val value = extractValue(subtext)
-                val name = extractName(subtext)
-                val timestamp = getCurrentTimestamp()
-
-                val notificationData = Expense(name, value, "Sem categoria", timestamp)
-                ApiClient.sendNotification(notificationData) { success ->
-                    if (success) {
-                        Log.d("NotificationListener", "Notification with data sent successfully!")
-                    } else {
-                        Log.e("NotificationListener", "Failed to send notification with data.")
-                    }
+            ApiClient.sendNotification(expense) { success ->
+                if (success) {
+                    Log.d("NotificationListener", "Notification with data sent successfully!")
+                } else {
+                    Log.e("NotificationListener", "Failed to send notification with data.")
                 }
             }
         }
@@ -78,27 +77,6 @@ class NotificationListener : NotificationListenerService() {
         Log.d("NotificationListener", "Notification removed: ${sbn?.packageName}")
     }
 
-    /**
-     * Extracts the value from the text like "R$7,95" to "795"
-     */
-    private fun extractValue(text: String): Int {
-        val regex = "R\\$([\\d,]+)".toRegex() // Regex to match R$ followed by numbers with commas
-        val match = regex.find(text)
-        return match?.groupValues?.get(1)?.replace(",", "")?.toInt() ?: 0
-    }
-
-    /**
-     * Extracts the name from the text (e.g., "UBER*PENDING")
-     */
-    private fun extractName(text: String): String {
-        val regex = "em\\s([A-Za-z0-9*]+)".toRegex() // Regex to match name after "em"
-        val match = regex.find(text)
-        return match?.groupValues?.get(1) ?: "Unknown"
-    }
-
-    /**
-     * Gets the current timestamp in the format yyyy-MM-dd HH:mm:ss
-     */
     private fun getCurrentTimestamp(): String {
         val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
         sdf.timeZone = TimeZone.getTimeZone("UTC")
