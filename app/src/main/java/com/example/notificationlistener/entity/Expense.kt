192b7c5f43c2ac26754df3ccd2c79ce9cc6a7913
Luís Felipe <luis.felipe@smgbit.com.br>
2025-03-13 23:31:35 -0300

    fix error

--- app/src/main/java/com/example/notificationlistener/entity/Expense.kt
+++ app/src/main/java/com/example/notificationlistener/entity/Expense.kt
@@ -3,6 +3,8 @@ package com.example.notificationlistener.entity
 data class Expense(
     val name: String,
     val value: Number,
+    val bank: String,
+    val card: String,
     val category: String,
     val timestamp: String,
 )
